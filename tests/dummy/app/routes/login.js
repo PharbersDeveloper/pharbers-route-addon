97c256fd1ebde3d9185fbf64bf527b5fbca06f85
Faiz <891366402@qq.com>
2018-09-08 17:12:08 +0800

    test

--- tests/dummy/app/routes/login.js
+++ tests/dummy/app/routes/login.js
@@ -39,81 +39,45 @@ export default Route.extend({
 
 		{
 			// Demo2
-			let result = this.store.createRecord('bmauth', {
-				id: "-1",
-				token: "fuck tokentokentokentoken",
-				phone: this.store.createRecord('bmphone', {
-					id: "-2",
-					phone: '10000000',
-				})
-			})
-
-			let values = [
-				{name: "Alex", type: 'wechat', list: ['a', 'b', 'c']},
-				{name: "Alfred", type: 'wechat', list: ['e', 'd', 'f']}
-			]
-
-			values.forEach((elem, index) => {
-				result.get(elem.type).pushObject(this.store.createRecord("bm"+elem.type, {
-					id: index,
-					name: elem.name,
-					list: elem.list
-				}))
-			});
-
-			let r = this.store.object2JsonApi('bmauth', result)
-			console.info(r)
+			// let result = this.store.createRecord('bmauth', {
+			// 	id: "-1",
+			// 	token: "fuck tokentokentokentoken",
+			// 	phone: this.store.createRecord('bmphone', {
+			// 		id: "-2",
+			// 		phone: '10000000',
+			// 	})
+			// })
+			//
+			// let values = [
+			// 	{name: "Alex", type: 'wechat', list: ['a', 'b', 'c']},
+			// 	{name: "Alfred", type: 'wechat', list: ['e', 'd', 'f']}
+			// ]
+			//
+			// values.forEach((elem, index) => {
+			// 	result.get(elem.type).pushObject(this.store.createRecord("bm"+elem.type, {
+			// 		id: index,
+			// 		name: elem.name,
+			// 		list: elem.list
+			// 	}))
+			// });
+			//
+			// let r = this.store.object2JsonApi('bmauth', result)
+			// console.info(r)
 		}
 
-
+		let result = this.store.peekAll('bmphone');
+		console.info(result)
 
 	},
 	actions: {
 		test() {
-            // let model = this.modelFor('login');
-            // let temp = model.save()
-
-            // 查询
-            // let res = "BMPhone";
-            // let inputs = [{id: "2", type: "eq_cond", key: "phone", val: "13720200891"}, {id: "3", type: "location", title: "天庭", location: "000.000，000.000"}]
-            //
-            // let data1 = this.Data;
-            // data1.init()
-            // data1.set('attributes.res', res);
-            // data1.set('relationships.conditions', {data: [{id: '2', type: 'eq_cond'}]});
-            // data1.set('relationships.location', {data: [{id: '3', type: 'location'}]});
-            //
-            // let condition1 = this.Contact.joint(data1, inputs)
-            // window.console.info(condition1)
-
-            // 算是新增吧
-            // let data2 = this.Data;
-            // data2.init()
-            // data2.set('attributes.name', 'Alex');
-            // data2.set('attributes.age', 18);
-            // data2.set('relationships.user', {data: [{id: '2', type: 'eq_cond'}]});
-            //
-            // let condition2 = this.Contact.joint(data2, [])
-            // window.console.info(condition2)
-
-		},
-
-        insert() {
-            let data = this.Data;
-            data.init();
-            data.set('type', 'Contact');
-            data.set('attributes.name', 'Alex');
-            data.set('attributes.phone', '18510971868');
-            data.set('relationships.location', {data: {id: '10010', type: 'Location'}});
-            data.set('relationships.orders', {data: [{id: '11112', type: 'Order'}, {id: '11113', type: 'Order'}]});
-
-            let inputs = [
-                {id: "10010", type: "Location", title: "中国", address: "北京", district: "通州"},
-                {id: "11112", type: "Order", title: "啦啦啦"},
-                {id: "11113", type: "Order", title: "叭叭叭"}
-            ]
-            let condition = this.Contact.joint(data, inputs)
-            this.store.transaction('contact', condition)
-        },
+			let result = this.store.queryRecord('bmphone', {id: 1});
+			result.then(elem => {
+					localStorage.setItem('recentContacts', elem);
+					console.info(elem)
+					this.store.set('recentContacts', elem);
+			})
+			console.info(result)
+		}
 	}
 });
